23b20f60260e21838fdaec6148c5a5daca202370
dslyvka <daniilslyvka@gmail.com>
2021-11-02 17:31:36 +0300

    hw 12 is done

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,11 @@ import debounce from 'lodash.debounce';
 import countryMarkup from '../countryMarkup.hbs';
 import countriesMarkup from '../countriesMarkup.hbs';
 
-// import { alert, defaultModules } from '../../node_modules/@pnotify/core/dist/PNotify.js';
-// import * as PNotifyMobile from '../../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import { error, defaults } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
-
-//   defaultModules.set(PNotifyMobile, {});
-
-// PNotify.alert({
-//   text: 'Notice me, senpai!',
-// });
+defaults.delay = 2000;
 
 const searchRef = document.querySelector('[data-action="search"]');
 const countryRef = document.querySelector('.country');
@@ -19,27 +15,48 @@ searchRef.addEventListener('input', debounce(renderCountry, 500));
 
 function onSearch() {
   if (searchRef.value.trim() === '') {
-    return fetch(`https://restcountries.com/v2/name/${searchRef.value}`)
-      .then((countryRef.innerHTML = ''))
-      .catch(error => console.log(error));
+    return fetch(`https://restcountries.com/v2/name/${searchRef.value}`).catch(error =>
+      console.log(error),
+    );
   }
-  return fetch(`https://restcountries.com/v2/name/${searchRef.value}`)
-    .then(response => response.json())
-    .catch(error => console.log(error));
+  return getInfo();
 }
 
 function renderCountry() {
   onSearch().then(data => {
     if (data.status === 404) {
-      countryRef.innerHTML = '';
+      clearMarkup();
+      return;
+    } else if (data.length >= 2 && data.length <= 10) {
+      clearMarkup();
+      makeMarkup(data);
+      return;
+    } else if (data.length > 10) {
+      clearMarkup();
+      error({
+        title: 'Too many matches found. Please, enter a more specific query!',
+      });
       return;
     }
-    if (data.length >= 2 && data.length <= 10) {
-      countryRef.innerHTML = '';
-      countryRef.insertAdjacentHTML('beforeend', countriesMarkup(data));
-      return;
-    }
-    countryRef.innerHTML = '';
-    countryRef.insertAdjacentHTML('beforeend', countryMarkup(data));
+
+    clearMarkup();
+    makeMarkup(data);
   });
+}
+
+function getInfo() {
+  return fetch(`https://restcountries.com/v2/name/${searchRef.value}`)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+}
+
+function clearMarkup() {
+  countryRef.innerHTML = '';
+}
+
+function makeMarkup(data) {
+  if (data.length >= 2 && data.length <= 10) {
+    countryRef.insertAdjacentHTML('beforeend', countriesMarkup(data));
+  }
+  else countryRef.insertAdjacentHTML('beforeend', countryMarkup(data));
 }
